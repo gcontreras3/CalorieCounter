d0e43b313ebf72c39664e3863d75def695f7ba6a
Jerry <ivan@Jerrys-Laptop.local>
2025-02-10 11:05:22 -0600

    added second regex function

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 
 let isError = false;
+
+function cleanInputString(str){
+    // console.log("original string: ", str);
+    // you need to use the \ character in regex to escape the + symbol because it has a specail meaning in regex
+    // In regex, shorthand character classes allow you to match specific characters
+    // regex can alter the pattern matching behavior, "global" will tell the pattern to continue looking after it has found a match.
+    const regex = /[+-\s]/g;
+    
+    return str.replace(regex, "");
+}
+// console.log(cleanInputString("+-99"));
+function isInvalidInput(str){
+    const regex = /[0-9]e[0-9]/i;
+}
