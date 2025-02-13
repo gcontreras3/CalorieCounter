bcc7fadead6b0cded5837de9820683bb90575831
gcontreras3 <gcontreras31996@gmail.com>
2025-02-13 12:28:07 -0600

    added paragraph elements to display calorie amounts

--- script.js
+++ script.js
@@ -69,7 +69,12 @@ function calculateCalories (e) {
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
-    output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${remainingCalories} Calorie ${surplusOrDeficit}</span>`;
+    output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>`;
+    
 }
 function getCaloriesFromInputs(list){
     let calories = 0;
